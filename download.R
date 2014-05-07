cbf96128d257aa4ee482da7c6d48fbbf9f3db310
Travis <travismay@gmail.com>
2014-05-07 15:18:55 -0500

    Create plot1.R

--- download.R
+++ download.R
@@ -1,11 +1,14 @@
-if(!file.exists("data")) {
-  dir.create("data")
-}
-
 fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 temp <- tempfile()
-download.file(fileUrl, destfile = temp)
-data <- read.table(unz(temp, "household_power_consumption.txt"))
-unlink(temp)
 
-data <- 
+if(!file.exists("data")) {
+  dir.create("data")
+  
+  download.file(fileUrl, destfile = temp)
+  data <- read.table(unz(temp, "household_power_consumption.txt"))
+  unlink(temp)
+}
+
+
+
+
