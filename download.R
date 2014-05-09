974dd9867df084f3dbfa56661fd585fe70196227
Travis <travismay@gmail.com>
2014-05-09 10:29:25 -0500

    Finish download.R

--- download.R
+++ download.R
@@ -1,36 +1,25 @@
-fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-temp <- tempfile()
-
-if(!file.exists("data")) {
-  dir.create("data")
-  
-  download.file(fileUrl, destfile = temp)
-  data <- read.table(unz(temp, "household_power_consumption.txt"))
-  unlink(temp)
-}
-
-####  Function that downloads file from specified url and unzips into a destination directory.
-####  Function will not run if a directory exists.
+####  Function downloads file from specified url and unzips the downloaded file into a destination directory.
+####  Function will not run if a directory of data previously exists.
 
 download <- function(url, dataFileName, zipFileName) {
 
   #   url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
   #   dataFileName <- "household_power_consumption.txt"
-  #   zipFilename <- "household_power_consumption.zip"
+  #   zipFileName <- "household_power_consumption.zip"
   
   ## Establish destinations for zip and data files.
   dataDirPath <- "data"
   datasetDirPath <- file.path(dataDirPath, dataFileName)
-  zipPath <- file.path(dataDirPath, zipFilename)
+  zipPath <- file.path(dataDirPath, zipFileName)
   
-  
-  ## Downloading/Unzipping data
+  ## Downloading/Unzipping data iff data doesn't already exist
   if(!file.exists(dataDirPath)){ 
     dir.create(dataDirPath) 
   }
   
+  ## 
   if(!file.exists(zipPath)){ 
-    download.file(Url, zipPath, mode="wb")
+    download.file(url, zipPath, mode="wb")
   }
   
   if(!file.exists(datasetDirPath)) { 
@@ -40,3 +29,13 @@ download <- function(url, dataFileName, zipFileName) {
 }
 
 
+# fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+# temp <- tempfile()
+# 
+# if(!file.exists("data")) {
+#   dir.create("data")
+#   
+#   download.file(fileUrl, destfile = temp)
+#   data <- read.table(unz(temp, "household_power_consumption.txt"))
+#   unlink(temp)
+# }
