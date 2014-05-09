1b66c54a14e48120c8cd16cdcd9593af3f468e8e
Travis <travismay@gmail.com>
2014-05-09 09:58:16 -0500

    Modify download.R

--- download.R
+++ download.R
@@ -9,6 +9,23 @@ if(!file.exists("data")) {
   unlink(temp)
 }
 
+####  Function that downloads file from specified url and unzips into a destination directory.
+####  Function will not run if a directory exists.
 
+download <- function(url, dataDirPath, dataSetDirPath, zipFileName,) {
+  
+  ## Check if dir for data exists. Create dir if not.
+  if (!file.exists("data.zip")) {
+    dir.create("data")
+    
+    ## Download file
+    fileUrl <- url
+    download.file(fileUrl,destination)
+    
+    ## Unzip file
+    unzip(destination, exdir="./data")
+  }
+  
+}
 
 
