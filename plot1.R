6e58391199c2a5d3084cd727d5de0ec35ce1b281
Travis <travismay@gmail.com>
2014-05-09 10:59:59 -0500

    Modify plot1.R to use return value of download function

--- plot1.R
+++ plot1.R
@@ -1,4 +1,17 @@
 library(data.table)
 source("download.R")
+setwd("~/ExData_Plotting1")
 
-download()
+
+## Assign values to variables that are used for arguments in the download function.
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+dataFileName <- "household_power_consumption.txt"
+zipFileName <- "household_power_consumption.zip"
+
+## Download function takes url of desired file, desired file name, and name of zip file.
+file <- download(url, dataFileName, zipFileName)
+
+## Read in file, but only read in observations from 2007-02-01 through 2007-02-02.
+data <- fread(file)
+
+head(data)
